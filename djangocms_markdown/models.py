f765e039135238c2d9ac63edd40cdaa2ffbb4da9
Olle Vidner <olle@vidner.se>
2014-08-21 14:01:31 +0200

    Added __str__

--- djangocms_markdown/models.py
+++ djangocms_markdown/models.py
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
 from cms.models import CMSPlugin
 
 
+@python_2_unicode_compatible
 class Markdown(CMSPlugin):
     body = models.TextField()
+
+    def __str__(self):
+        return self.text[:64]
