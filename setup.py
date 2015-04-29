52d52b46c237c91b572c3d9eebe8ac3aff6dcdee
Nicolas Noé <nicolas@niconoe.eu>
2015-04-29 16:12:12 +0200

    Small fix in setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setup(
     include_package_data=True,
     install_requires=[
         'Django',
-        'South',
         'django-cms>=3.0.0',
         'django-markdown-deux',
     ], extras_require = {
